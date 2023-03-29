78307eef295b0a4257eec09a32a835f8254407fe
umgefahren <55623006+umgefahren@users.noreply.github.com>
2023-03-29 19:23:48 +0200

    Made buildable again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
@@ -13,6 +11,7 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
