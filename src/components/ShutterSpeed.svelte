1e3a45e4c1bf126e8cfd824345c968afcf7ae6d0
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-08-14 16:29:42 +0200

    Improved everything

---
+++ src/components/ShutterSpeed.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let shutterSpeed: string;
+  console.log(shutterSpeed.split("/"))
+</script>
+
+{#if shutterSpeed.split("/").length === 2}
+  {@const [numerator, denominator] = shutterSpeed.split("/")}
+  <sup>{numerator}</sup>&frasl;<sub>{denominator}</sub>
+{:else}
+    {shutterSpeed}"
+{/if}
